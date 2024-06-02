f5f577386906b4a86d0c1fc3e5be2b12f6bdc81c
EOEboh <ecolejnr007@gmail.com>
2024-06-02 20:06:42 +0100

    basic view

--- src/components/ScreenRecorder.tsx
+++ src/components/ScreenRecorder.tsx
@@ -3,55 +3,66 @@ import useRecord from "../hooks/useRecord";
 import { useEffect, useState } from "react";
 
 const ScreenRecorder = () => {
-  const [startScreenRecording, Recorder, screenRecording] = useRecord();
+  const [startScreenRecording, recorder, screenRecording, displayMedia] =
+    useRecord();
 
   const [recordState, setRecordState] = useState<string | undefined>("");
 
   useEffect(() => {
-    setRecordState(Recorder?.state);
-  }, [Recorder?.state, recordState]);
+    setRecordState(recorder?.state);
+  }, [recorder?.state, recordState]);
 
   console.log("recordState", recordState);
+  console.log(navigator.mediaDevices.getSupportedConstraints());
+
+  const stopRecording = () => {
+    recorder?.stop();
+    displayMedia?.stop();
+    setRecordState(recorder?.state);
+  };
 
   const renderButtonState = () => {
-    switch (recordState) {
-      case "recording":
-        return (
-          <button
-            onMouseDown={() => {
-              Recorder && Recorder.stop();
-              setRecordState(Recorder?.state);
-            }}
-          >
-            Recording...
-          </button>
-        );
-        break;
-      case "inactive":
-        return <button onMouseDown={() => {}}>Preview</button>;
-        break;
-      case undefined:
-        return (
-          <button onMouseDown={() => startScreenRecording()}>
-            Start Recording
-          </button>
-        );
-      default:
-        return (
-          <button onMouseDown={() => startScreenRecording()}>
-            Start Recording
-          </button>
-        );
-
-        break;
+    if ("MediaRecorder" in window) {
+      switch (recordState) {
+        case "recording":
+          return (
+            <div>
+              <button onMouseDown={stopRecording}>Stop Recording</button>
+            </div>
+          );
+          break;
+        case "inactive":
+          return (
+            <div className="flex gap-2">
+              <button onMouseDown={() => {}}>Re-record</button>
+              <button onMouseDown={() => {}}>Preview</button>
+            </div>
+          );
+          break;
+        case undefined:
+          return (
+            <button onMouseDown={() => startScreenRecording()}>
+              Start Recording
+            </button>
+          );
+        default:
+          return (
+            <button onMouseDown={() => startScreenRecording()}>
+              Start Recording
+            </button>
+          );
+          break;
+      }
+    } else {
+      return "Sorry, your browser doesn't support the MediaRecorder API, so this demo will not work";
     }
   };
 
   return (
-    <Fragment>{renderButtonState()}</Fragment>
-
-    //   <video ref={screenRecording} height={300} width={600} controls />
-    // </>
+    <Fragment>
+      {renderButtonState()}
+      <video ref={screenRecording} height={300} width={600} controls />
+    </Fragment>
   );
 };
 export default ScreenRecorder;
--- src/hooks/useRecord.ts
+++ src/hooks/useRecord.ts
@@ -12,40 +12,62 @@ const useRecord = () => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const screenRecording = useRef<any | null>(null);
 
-  const [Recorder, setRecorder] = useState<MediaRecorder>();
+  const [recorder, setRecorder] = useState<MediaRecorder | undefined>();
   const [displayMedia, setDisplayMedia] = useState<MediaStreamTrack>();
 
   const startScreenRecording = async () => {
     const stream = await navigator.mediaDevices.getDisplayMedia({
-      audio: true,
-      video: true,
+      video: {
+        displaySurface: "window",
+        width: { max: 1920 },
+        height: { max: 1080 },
+        frameRate: { max: 30 },
+        aspectRatio: 1.777777778, // 16:9 aspect ratio
+      },
+      audio: {
+        echoCancellation: true,
+        noiseSuppression: true,
+        sampleRate: 44100,
+      },
     });
     console.log("stream", stream.active);
     const mediaRecorder = new MediaRecorder(stream);
     setRecorder(mediaRecorder);
     setDisplayMedia(stream.getVideoTracks()[0]);
+
     const screenRecordingChunks: Array<ScreenRecordingChunksType> = [];
-    mediaRecorder.ondataavailable = (e) => {
-      if (e.data.size > 0) {
-        screenRecordingChunks.push(e.data);
-        console.log("screenrecording", e.data);
+
+    mediaRecorder.ondataavailable = (event) => {
+      if (typeof event.data === "undefined") return;
+      if (event.data.size === 0) return;
+      if (event.data.size > 0) {
+        screenRecordingChunks.push(event.data);
+        console.log("screenrecording", event.data);
       }
     };
+
+    //Start the recording.
+    mediaRecorder.start();
+
     mediaRecorder.onstop = () => {
       //onstop event of media recorder
+      if (displayMedia) {
+        displayMedia.stop();
+      }
       const blob: Blob = new Blob(screenRecordingChunks as BlobPart[], {
         type: "video/webm",
       });
       const url = URL.createObjectURL(blob);
       screenRecording.current.src = url;
-      if (displayMedia) {
-        displayMedia.stop();
-      }
+      console.log("screenrecording2", screenRecording);
     };
-    //Start the recording.
-    mediaRecorder.start();
   };
-  return [startScreenRecording, Recorder, screenRecording] as const;
+  return [
+    startScreenRecording,
+    recorder,
+    screenRecording,
+    displayMedia,
+  ] as const;
 };
 
 export default useRecord;
